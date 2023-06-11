9adf25917919d069e9982956813159d1b4c05fbe
phat181999 <phat181999@gmail.com>
2023-06-11 17:51:21 +0700

    psh

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    html = "<h1>HELLO WORLD I'M HO TAN PHAT FROM VIET NAM</h1>"
-    return html.format(format)
+    return "<h1 style='text-align: center;'>Welcome to my Cloud DevOps Engineer Capstone Project!</h1>"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
