3300dbdc3fc6d9c4ce4bbfd5a0fc85c056cbb27b
phat181999 <phat181999@gmail.com>
2023-06-11 23:26:24 +0700

    haha

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "<h1 style='text-align: center;'>Welcome to my Cloud DevOps Engineer Capstone Project!</h1>"
+    html = "<h3>Udacity ND capstone project by Walid</h3>"
+    return html.format(format)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
